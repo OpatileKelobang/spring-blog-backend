5dec541f9b46ff79b7dff54d8a2b77e9feca3579
OpatileKelobang <49315650+OpatileKelobang@users.noreply.github.com>
2021-04-21 08:28:21 +0200

    Initial commit

---
+++ src/main/java/bw/co/bogote/springblogbackend/SpringBlogBackendApplication.java
@@ -0,0 +1,13 @@
+package bw.co.bogote.springblogbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlogBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBlogBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bw/co/bogote/springblogbackend/SpringBlogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package bw.co.bogote.springblogbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBlogBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
